75f0a5fbd4bc0d00a59457635d46360d6fba49c7
odin-lific <82076270+odin-lific@users.noreply.github.com>
2021-04-20 13:13:59 +0900

    alertDialog sample

--- app/src/main/java/com/odin/composesample/ui/views/AlertDialogSample.kt
+++ app/src/main/java/com/odin/composesample/ui/views/AlertDialogSample.kt
@@ -1,4 +1,79 @@
 package com.odin.composesample.ui.views
 
-class AlertDialogSample {
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+
+class AlertDialogSample: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AlertDialogSample()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun AlertDialogSample() {
+        MaterialTheme {
+            Column {
+                val openDialog = remember { mutableStateOf(false)  }
+
+                Button(onClick = {
+                    openDialog.value = true
+                }) {
+                    Text("Click")
+                }
+
+                if (openDialog.value) {
+
+                    AlertDialog(
+                        onDismissRequest = {
+                            // Dismiss the dialog when the user clicks outside the dialog or on the back
+                            // button. If you want to disable that functionality, simply use an empty
+                            // onCloseRequest.
+                            openDialog.value = false
+                        },
+                        title = {
+                            Text(text = "Dialog Title")
+                        },
+                        text = {
+                            Text("Here is a text ")
+                        },
+                        confirmButton = {
+                            Button(
+
+                                onClick = {
+                                    openDialog.value = false
+                                }) {
+                                Text("This is the Confirm Button")
+                            }
+                        },
+                        dismissButton = {
+                            Button(
+
+                                onClick = {
+                                    openDialog.value = false
+                                }) {
+                                Text("This is the dismiss Button")
+                            }
+                        }
+                    )
+                }
+            }
+
+        }
+    }
+
 }
